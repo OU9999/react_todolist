b183993913841c0d21c3cc2c2ce8a1a5d3db21ed
오유진 <omh232323@gmail.com>
2023-01-01 17:39:26 +0900

    ho

---
+++ src/Root.tsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import ToDoList from "./toDoList";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body {  
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color:${(props) => props.theme.textColor}
+    }
+  *{
+    box-sizing: border-box;
+  }
+`;
+
+function Root() {
+  return (
+    <>
+      <GlobalStyle />
+      <ToDoList />
+    </>
+  );
+}
+
+export default Root;
